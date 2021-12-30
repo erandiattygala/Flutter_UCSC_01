34157f64cf89e0945262b2d306e5bc55a2d41d4f
Erandi Attyagala <94741517+erandiattygala@users.noreply.github.com>
2021-12-30 17:08:32 +0530

    div app theme (#4)
    App Theme

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -3,23 +3,25 @@ import 'package:flutter/material.dart';
 import 'package:demo/views/home.dart';
 import 'package:demo/views/users.dart';
 import 'package:demo/views/user_profile.dart';
+import 'package:demo/commons/themes.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo-Fonts',
       initialRoute: '/',
       onGenerateRoute: RouteGenerator.generateRoute,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+     // theme: ThemeData(
+      //primarySwatch: Colors.blue,
+      //),
+      theme: darkTheme,
       home: HomePage(title: "Home"),
      // home: const HomePage(title: 'Flutter Demo Home Page'),
     );
